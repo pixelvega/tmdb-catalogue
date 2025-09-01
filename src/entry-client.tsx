7f072c51e72b97ed773625603568217d58d9e6fa
pixelvega <pixelvega.info@gmail.com>
2025-09-01 22:25:19 +0200

    styles: add SCSS and main styles

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom"
 
 import { Router } from "./router"
 
-import "./index.css"
+import "./styles/main.scss"
 
 ReactDOM.hydrateRoot(
   document.getElementById("root") as HTMLElement,
--- src/layout/BaseLayout.tsx
+++ src/layout/BaseLayout.tsx
@@ -1,11 +1,55 @@
+import logo from "../assets/logo.svg"
+import { Link, NavLink } from "react-router-dom"
+
+import "./BaseLayout.scss"
+
 interface Props {
   children: React.ReactNode
 }
 
 const BaseLayout = ({ children }: Props) => {
   return (
-    <div>
-      <h1>Base Layout</h1>
+    <div className="base-layout">
+      <header className="header">
+        <div className="header__logo" aria-label="TMDB Max">
+          <Link to="/" aria-label="Ir a la página principal de TMDB Max">
+            <img
+              src={logo}
+              alt=""
+              aria-hidden="true"
+              className="header__logo-icon"
+            />
+          </Link>
+        </div>
+        <nav className="header__nav">
+          <ul className="header__nav-list">
+            <li className="header__nav-list-item">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive
+                    ? "header__nav-list-item__link header__nav-list-item__link--active"
+                    : "header__nav-list-item__link"
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="header__nav-list-item">
+              <NavLink
+                to="/favourites"
+                className={({ isActive }) =>
+                  isActive
+                    ? "header__nav-list-item__link header__nav-list-item__link--active"
+                    : "header__nav-list-item__link"
+                }
+              >
+                Favourites
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <div>{children}</div>
     </div>
   )
